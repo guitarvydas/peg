9c0ee2b5beedd3294439b3f7c59a1ef314990b36
Paul Tarvydas <paul.tarvydas@gmail.com>
2013-07-29 17:13:34 -0400

    revamped regression testing ; braces in strings still not working

---
+++ bugs.lisp
@@ -0,0 +1,6 @@
+;; a collection of edge-cases that, at one time, broke the PEG parser
+
+(defun bugs ()
+  (pprint (esrap:parse 'peg-grammar::grammar "x <- '1' {(format nil \"~a\" 1)}"))
+  (pprint (esrap:parse 'peg-grammar::grammar "x <- '1' {(format nil {~a} 1)}")))
+
